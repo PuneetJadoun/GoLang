43efce65e75f08d3687e2c8067334f13a3ec8fe3
Puneet Kumar Singh <punitku624@gmail.com>
2026-04-27 21:00:47 +0530

    constants in go

---
+++ constants/constants.go
@@ -0,0 +1,21 @@
+package main
+
+
+import "fmt"
+
+//const age  = 10
+
+func main(){
+
+	// const name = "golang"
+	// const age = 30
+
+
+	// group constants
+	// const (
+	// 	port  = 50
+	// 	host = "localhost"
+	// )
+
+	
+}
