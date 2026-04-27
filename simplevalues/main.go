3fe528bd146bcd614a0028e6026fb1a42a3e9e2c
Puneet Kumar Singh <punitku624@gmail.com>
2026-04-27 20:28:39 +0530

    simple values folder added

---
+++ simplevalues/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main(){
+	// simple values
+
+
+	// int 
+	fmt.Println(1)
+
+
+	// string
+	fmt.Println("This is a string")
+
+
+	// bool
+	fmt.Println(true)
+
+
+	// float
+	fmt.Println(10.5)
+
+}
