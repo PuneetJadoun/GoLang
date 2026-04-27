da14827e25d578519807707a7c025a7f4c6660be
Puneet Kumar Singh <punitku624@gmail.com>
2026-04-27 20:46:07 +0530

    variables in golang

---
+++ variables/variables.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// var name string = "golang"	
+
+
+	// infer type of variable
+	var name = "golang"
+
+
+    //	var isAdult bool = true
+	var isAdult = true
+	
+
+	// if variable is declared have to used in code
+
+	// var age int = 12
+	// var age = 10
+
+
+	// shorthand syntax
+
+	// name := "golang"   // used to decalare var and assign its value
+
+
+
+	// var name string
+
+	// name = "golang"
+
+
+	fmt.Println(name)
+	fmt.Println(isAdult)
+
+}
