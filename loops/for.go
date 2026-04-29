8cf7379c403d2e84c2ccf788669c0225a3814b46
Puneet Kumar Singh <punitku624@gmail.com>
2026-04-29 23:18:51 +0530

    loops

---
+++ loops/for.go
@@ -0,0 +1,44 @@
+package main
+
+
+//import "fmt"
+
+// for -> only constructure of loop in go
+
+func main() {
+	// while loop
+	// i:= 1
+
+	// for i <= 5 {
+	// 	fmt.Println(i)
+	// 	i++
+	//}
+
+	// infinite loop
+
+	// for {
+	// 	fmt.Println("infinite loop")
+	// }
+
+
+	// classic for loop
+	//for i := 1; i <= 5; i++ {
+
+		// break
+		// continue
+
+		// if(i == 3){
+		// 	continue
+		// }
+
+		//fmt.Println(i)
+	//}
+
+
+	// range
+	
+	// for i:= range 4{
+	// 	fmt.Println(i)
+	// }
+
+}
