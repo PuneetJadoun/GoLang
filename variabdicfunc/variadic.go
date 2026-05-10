d0d3270593a1c0fb9143b85255536a773c00d3a0
Puneet Kumar Singh <punitku624@gmail.com>
2026-05-10 22:31:42 +0530

    closures in go

--- variabdicfunc/variadic.go
+++ variabdicfunc/variadic.go
@@ -1,6 +1,23 @@
 package main
 
 
-func main(){
-	
+import "fmt"
+
+func sum(nums ...int) int{  // if want any type use interface{} instead of int
+	total := 0
+
+	for _, num := range nums{
+		total = total + num
+	}
+	return total
 }
+
+
+func main(){
+	//fmt.Println(1, 2, 3, 4 , 5 , "h")
+
+	nums:= []int{1, 2, 3, 4, 5}
+	//result := sum(1, 2, 3, 4)
+	result := sum(nums...)  // if want any type use interface{} instead of int	
+	fmt.Println(result)
+} 
